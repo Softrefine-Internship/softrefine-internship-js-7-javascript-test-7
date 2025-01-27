387bf1b5dfdbf75d7aad463fdd5c6ddc214976df
Tirth <tirth1943@gamil.com>
2025-01-27 13:19:37 +0530

    Test 07: Started Day 2

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// write javascript here
+// script.js
+const API_CATEGORY_URL = "https://opentdb.com/api_category.php";
+getCategory();
+
+async function getCategory() {
+  const response = await fetch(API_CATEGORY_URL); // Use a different variable name, e.g., 'response'
+  const data = await response.json(); // Use 'response' here
+  
+  console.log(data.trivia_categories);
+  return data.trivia_categories;
+}
